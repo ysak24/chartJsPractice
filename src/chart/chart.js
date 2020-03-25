8d9b4b009a7cdb3cf777ee62e5d922aa82d075a7
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-25 15:03:33 +0900

    config 移動および js → json

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -1,10 +1,10 @@
 import Chart from 'chart.js'
-import config from '../../config/chart.js'
+import config from './config'
 import organizeData from './organizeData.js'
 
 const ctx = document.getElementById('myChart').getContext('2d')
 new Chart(ctx, {
-    type: config['chartType'],
+    type: config['type'],
     data: organizeData(),
     options: config['options']
 })
