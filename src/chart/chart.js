600a0504703d723b89ff47e02d0f9ba467f36400
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-25 15:12:33 +0900

    なんとなく記法変更

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -4,7 +4,7 @@ import organizeData from './organizeData.js'
 
 const ctx = document.getElementById('myChart').getContext('2d')
 new Chart(ctx, {
-    type: config['type'],
+    type: config.type,
     data: organizeData(),
-    options: config['options']
+    options: config.options
 })
