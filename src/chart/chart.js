d81fecf31cd8d26552f9e641714865ea817e9ba0
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-25 14:25:48 +0900

    chart 分離

---
+++ src/chart/chart.js
@@ -0,0 +1,10 @@
+import Chart from 'chart.js'
+import config from '../../config/chart.js'
+import organizeData from './organizeData.js'
+
+const ctx = document.getElementById('myChart').getContext('2d')
+new Chart(ctx, {
+    type: config['chartType'],
+    data: organizeData(),
+    options: config['options']
+})
--- src/index.js
+++ src/index.js
@@ -3,13 +3,5 @@ import 'whatwg-fetch'
 import 'core-js'
 import 'regenerator-runtime/runtime'
 
-import Chart from 'chart.js'
-import config from '../config/chart.js'
-import organizeData from './organizeData.js'
-
-const ctx = document.getElementById('myChart').getContext('2d')
-new Chart(ctx, {
-    type: config['chartType'],
-    data: organizeData(),
-    options: config['options']
-})
+// チャート
+import './chart/chart.js'
