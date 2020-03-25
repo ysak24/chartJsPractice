3358feab095b1a0d9e36bbeddf6f8c9d7037289c
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-25 12:47:35 +0900

    不要なコードなど微調整

--- src/chart.js
+++
@@ -1,9 +0,0 @@
-// import config from '../config/chart.js'
-// import organizeData from './organizeData.js'
-
-// const ctx = document.getElementById('myChart').getContext('2d')
-// new Chart(ctx, {
-//     type: config['chartType'],
-//     data: organizeData(),
-//     options: config['options']
-// })
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import 'whatwg-fetch'
 import 'core-js'
 import 'regenerator-runtime/runtime'
 
-import 'chart.js'
-
+import Chart from 'chart.js'
 import config from '../config/chart.js'
 import organizeData from './organizeData.js'
 
 const ctx = document.getElementById('myChart').getContext('2d')
-const chart = new Chart(ctx, {
+new Chart(ctx, {
     type: config['chartType'],
     data: organizeData(),
     options: config['options']
 })
-
-console.log(chart)
