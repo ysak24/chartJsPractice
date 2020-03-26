bf3add6952aeefb2088bdf4a7fc2751251f391d8
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-26 14:24:26 +0900

    ボタンのイベントをあるべき場所へ

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -2,11 +2,12 @@ import Chart from 'chart.js'
 import config from './config'
 import organizeData from './organizeData.js'
 
-function drawChart(data, type) {
+drawChart()
+function drawChart(type) {
     const ctx = document.getElementById('myChart').getContext('2d')
     window.myChart = new Chart(ctx, {
         type: type ? type : config.defaultType,
-        data: data ? data : organizeData(),
+        data: organizeData(),
     })
 
     for (const props of Object.keys(config)) {
@@ -34,20 +35,16 @@ function drawChart(data, type) {
         myChart.data.datasets[0].borderWidth = config.borderWidth
     }
 }
-drawChart(organizeData())
 
 function redraw(type) {
     if (myChart && myChart.destroy) {
         myChart.destroy()
     }
-    drawChart(organizeData(), type)
+    drawChart(type)
 }
 
-document.getElementById('default').onclick = function () { redraw() }
-document.getElementById('line').onclick = function () { redraw('line') }
-document.getElementById('bar').onclick = function () { redraw('bar') }
-document.getElementById('radar').onclick = function () { redraw('radar') }
-document.getElementById('pie').onclick = function () { redraw('pie') }
-document.getElementById('polarArea').onclick = function () { redraw('polarArea') }
-document.getElementById('bubble').onclick = function () { redraw('bubble') }
-document.getElementById('scatter').onclick = function () { redraw('scatter') }
+const chartControl = {
+    drawChart,
+    redraw
+}
+export default chartControl
--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import 'whatwg-fetch'
 import 'core-js'
 import 'regenerator-runtime/runtime'
 
-import './chart/chart.js'
+import chartControl from './chart/chart.js'
+
+document.getElementById('default').onclick = function () { chartControl.redraw() }
+document.getElementById('line').onclick = function () { chartControl.redraw('line') }
+document.getElementById('bar').onclick = function () { chartControl.redraw('bar') }
+document.getElementById('radar').onclick = function () { chartControl.redraw('radar') }
+document.getElementById('pie').onclick = function () { chartControl.redraw('pie') }
+document.getElementById('polarArea').onclick = function () { chartControl.redraw('polarArea') }
+document.getElementById('bubble').onclick = function () { chartControl.redraw('bubble') }
+document.getElementById('scatter').onclick = function () { chartControl.redraw('scatter') }
