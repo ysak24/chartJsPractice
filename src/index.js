84913097725b8fecf3859919b236b12a70ae20ca
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-26 18:03:45 +0900

    手抜き部分微修正

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import 'regenerator-runtime/runtime'
 
 import chartControl from './chart/chart.js'
 
-document.getElementById('drawOptions').onclick = function () { chartChange() }
+document.getElementById('default').onclick = function () { chartChange() }
+document.getElementById('line').onclick = function () { chartChange() }
+document.getElementById('bar').onclick = function () { chartChange() }
+document.getElementById('radar').onclick = function () { chartChange() }
+document.getElementById('pie').onclick = function () { chartChange() }
+document.getElementById('polarArea').onclick = function () { chartChange() }
+document.getElementById('bubble').onclick = function () { chartChange() }
+document.getElementById('scatter').onclick = function () { chartChange() }
+
+document.getElementById('telework').onclick = function () { chartChange() }
+document.getElementById('personal').onclick = function () { chartChange() }
+
 function chartChange() {
     const form = document.getElementById('drawOptions')
     chartControl.redraw({ type: form.type.value, mode: form.mode.value })
