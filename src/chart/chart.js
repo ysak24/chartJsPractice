8ab2b72e7f07951e0e54801df45d9e68a171c5f4
小山 慶晋 <koyama.yoshiaki@pnc.jp>
2020-03-26 13:09:53 +0900

    再描画, type 変更のサンプル実装

--- src/chart/chart.js
+++ src/chart/chart.js
@@ -2,12 +2,31 @@ import Chart from 'chart.js'
 import config from './config'
 import organizeData from './organizeData.js'
 
-const ctx = document.getElementById('myChart').getContext('2d')
-const myChart = new Chart(ctx, {
-    type: config.type,
-    data: organizeData(),
-})
+function drawChart(data, type) {
+    const ctx = document.getElementById('myChart').getContext('2d')
+    window.myChart = new Chart(ctx, {
+        type: type ? type : 'bar',
+        data: data ? data : organizeData(),
+    })
 
-for (const props of Object.keys(config)) {
-    myChart[props] = config[props]
+    for (const props of Object.keys(config)) {
+        window.myChart[props] = config[props]
+    }
 }
+drawChart(organizeData())
+
+function redraw(type) {
+    if (myChart && myChart.destroy) {
+        myChart.destroy()
+    }
+    drawChart(organizeData(), type)
+}
+
+document.getElementById('default').onclick = function () { redraw() }
+document.getElementById('line').onclick = function () { redraw('line') }
+document.getElementById('bar').onclick = function () { redraw('bar') }
+document.getElementById('radar').onclick = function () { redraw('radar') }
+document.getElementById('pie').onclick = function () { redraw('pie') }
+document.getElementById('polarArea').onclick = function () { redraw('polarArea') }
+document.getElementById('bubble').onclick = function () { redraw('bubble') }
+document.getElementById('scatter').onclick = function () { redraw('scatter') }
--- src/chart/organizeData.js
+++ src/chart/organizeData.js
@@ -1,35 +1,46 @@
 // 取得したデータを chart で扱える形式に整理する。
 function organizeData() {
-    const returnValue = {}
-    returnValue.labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+    const data = {}
+    data.labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
 
-    returnValue.datasets = []
-    returnValue.datasets.push({})
-    returnValue.datasets[0].label = '# of Votes'
-    returnValue.datasets[0].data = [12, 19, 3, 5, 2, 3]
-    returnValue.datasets[0].backgroundColor = [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ]
-    returnValue.datasets[0].borderColor = [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ]
-    returnValue.datasets[0].borderWidth = 1
+    data.datasets = []
+    data.datasets.push({})
+    data.datasets[0].label = '# of Votes'
+    data.datasets[0].data = getData(data.labels.length)
 
-    returnValue.datasets.push({})
-    returnValue.datasets[1].label = '# additional info'
-    returnValue.datasets[1].data = [3, 2, 12, 14, 11, 12]
+    // 見た目に関する部分なのでここじゃない方がいい気がする
+    // data.datasets[0].backgroundColor = [
+    //     'rgba(255, 99, 132, 0.2)',
+    //     'rgba(54, 162, 235, 0.2)',
+    //     'rgba(255, 206, 86, 0.2)',
+    //     'rgba(75, 192, 192, 0.2)',
+    //     'rgba(153, 102, 255, 0.2)',
+    //     'rgba(255, 159, 64, 0.2)'
+    // ]
+    // data.datasets[0].borderColor = [
+    //     'rgba(255, 99, 132, 1)',
+    //     'rgba(54, 162, 235, 1)',
+    //     'rgba(255, 206, 86, 1)',
+    //     'rgba(75, 192, 192, 1)',
+    //     'rgba(153, 102, 255, 1)',
+    //     'rgba(255, 159, 64, 1)'
+    // ]
+    // data.datasets[0].borderWidth = 1
 
-    console.log(returnValue)
-    return returnValue
+    data.datasets.push({})
+    data.datasets[1].label = '# additional info'
+    data.datasets[1].data = getData(data.labels.length)
+    // data.datasets[1].borderWidth = 1
+
+    console.log(data)
+    return data
 }
 export default organizeData
+
+function getData(length) {
+    const data = []
+    for (let i = 0; i < length; i++) {
+        data.push(Math.floor(Math.random() * (20 - 1) + 1))
+    }
+    return data
+}
